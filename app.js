78ec11ad8979436231501ad94e05d477e4b7eaa3
Petterson Grande <grande.petterson@gmail.com>
2023-11-13 23:51:04 -0500

    Added gamepage and adjusted CSS features on info and maximize button, added functionality to playBtn: prevent default and now it takes you to gamepage

---
+++ app.js
@@ -0,0 +1,20 @@
+// SELECTORS
+
+const playBtn   = document.querySelector(".playBtn");
+const infoBtn   = document.querySelector("#infBtn");
+const maxBtn    =  document.querySelector("#maxBtn");
+
+
+//  FUNCTIONATILITIES
+
+// Preventing Default
+
+playBtn.addEventListener("click",(e) =>{
+
+    e.preventDefault();
+    window.location.href = '/gamepage.html';
+
+
+});
+
+
